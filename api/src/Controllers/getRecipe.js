9ed8b19c060c159c9f3706cff81c5167ea128a1e
Your Name <you@example.com>
2023-06-23 13:14:59 -0500

    Form Finalized

--- api/src/Controllers/getRecipe.js
+++ api/src/Controllers/getRecipe.js
@@ -1,79 +1,6 @@
-// require('dotenv').config();
-// const axios = require("axios");
-// const { API, API_D, API_T , API_F } = process.env;
-// const { Recipe, Diet } = require("../db.js");
-
-// const getRecipe = async () => {
-//   try {
-//     const response = await axios.get(API_D);
-//     const results = response.data.results.map(recipe => ({
-//       id: recipe.id,
-//       title: recipe.title,
-//       image: recipe.image,
-//       summary: recipe.summary.replace(/<[^>]+>/g, ""),
-//       healthScore: recipe.healthScore,
-//       steps: recipe.analyzedInstructions[0]?.steps?.map(step => step.step) || [],
-//       diets: recipe.diets.join(', '),
-//       createdInDb: recipe.createdInDb
-//     }));
-//     return results;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error(error);
-//   }
-// }
-
-// const getDb = async (req, res) => {
-//   try {
-//     const recipeDb = await Recipe.findAll({
-//       include: {
-//         model: Diet,
-//         attributes: ["name"],
-//         through: {
-//           attributes: [],
-//         },
-//       }
-//     });
-//     return recipeDb;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error('Error al obtener las recetas de la base de datos');
-//   }
-// }
-
-// const getAllRecipes = async (req, res) => {
-//   try {
-//     const api = await getRecipe(req, res);
-//     const db = await getDb(req, res);
-//     const allRecipes = api.concat(db);
-//     res.json(allRecipes);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: 'Error al obtener todas las recetas' });
-//   }
-// }
-
-// const idName = async () => {
-//   try {
-//     const api = await getRecipe();
-//     const db = await getDb();
-//     const all = api.concat(db);
-//     return all;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error('Error al obtener las recetas');
-//   }
-// }
-
-// module.exports = {
-//   getAllRecipes,
-//   idName,
-// }
-
-
 require('dotenv').config();
 const axios = require("axios");
-const { API, API_D, API_T, API_F } = process.env;
+const { API, API_D, API_T , API_F } = process.env;
 const { Recipe, Diet } = require("../db.js");
 
 const getRecipe = async () => {
@@ -91,12 +18,12 @@ const getRecipe = async () => {
     }));
     return results;
   } catch (error) {
-    console.error('Error al obtener las recetas de la API:', error.message);
-    throw new Error('Error al obtener las recetas de la API');
+    console.log(error);
+    throw new Error(error);
   }
-};
+}
 
-const getDb = async () => {
+const getDb = async (req, res) => {
   try {
     const recipeDb = await Recipe.findAll({
       include: {
@@ -105,26 +32,26 @@ const getDb = async () => {
         through: {
           attributes: [],
         },
-      },
+      }
     });
     return recipeDb;
   } catch (error) {
-    console.error('Error al obtener las recetas de la base de datos:', error.message);
+    console.log(error);
     throw new Error('Error al obtener las recetas de la base de datos');
   }
-};
+}
 
 const getAllRecipes = async (req, res) => {
   try {
-    const api = await getRecipe();
-    const db = await getDb();
+    const api = await getRecipe(req, res);
+    const db = await getDb(req, res);
     const allRecipes = api.concat(db);
     res.json(allRecipes);
   } catch (error) {
-    console.error('Error al obtener todas las recetas:', error.message);
+    console.log(error);
     res.status(500).json({ error: 'Error al obtener todas las recetas' });
   }
-};
+}
 
 const idName = async () => {
   try {
@@ -133,12 +60,88 @@ const idName = async () => {
     const all = api.concat(db);
     return all;
   } catch (error) {
-    console.error('Error al obtener las recetas:', error.message);
+    console.log(error);
     throw new Error('Error al obtener las recetas');
   }
-};
+}
 
 module.exports = {
   getAllRecipes,
   idName,
-};
+}
+
+
+//----------
+
+
+// require('dotenv').config();
+// const axios = require("axios");
+// const { API, API_D, API_T, API_F } = process.env;
+// const { Recipe, Diet } = require("../db.js");
+
+// const getRecipe = async () => {
+//   try {
+//     const response = await axios.get(API_D);
+//     const results = response.data.results.map(recipe => ({
+//       id: recipe.id,
+//       title: recipe.title,
+//       image: recipe.image,
+//       summary: recipe.summary.replace(/<[^>]+>/g, ""),
+//       healthScore: recipe.healthScore,
+//       steps: recipe.analyzedInstructions[0]?.steps?.map(step => step.step) || [],
+//       diets: recipe.diets.join(', '),
+//       createdInDb: recipe.createdInDb
+//     }));
+//     return results;
+//   } catch (error) {
+//     console.error('Error al obtener las recetas de la API:', error.message);
+//     throw new Error('Error al obtener las recetas de la API');
+//   }
+// };
+
+// const getDb = async () => {
+//   try {
+//     const recipeDb = await Recipe.findAll({
+//       include: {
+//         model: Diet,
+//         attributes: ["name"],
+//         through: {
+//           attributes: [],
+//         },
+//       },
+//     });
+//     return recipeDb;
+//   } catch (error) {
+//     console.error('Error al obtener las recetas de la base de datos:', error.message);
+//     throw new Error('Error al obtener las recetas de la base de datos');
+//   }
+// };
+
+// const getAllRecipes = async (req, res) => {
+//   try {
+//     const api = await getRecipe();
+//     const db = await getDb();
+//     const allRecipes = api.concat(db);
+//     res.json(allRecipes);
+//   } catch (error) {
+//     console.error('Error al obtener todas las recetas:', error.message);
+//     res.status(500).json({ error: 'Error al obtener todas las recetas' });
+//   }
+// };
+
+// const idName = async () => {
+//   try {
+//     const api = await getRecipe();
+//     const db = await getDb();
+//     const all = api.concat(db);
+//     return all;
+//   } catch (error) {
+//     console.error('Error al obtener las recetas:', error.message);
+//     throw new Error('Error al obtener las recetas');
+//   }
+// };
+
+// module.exports = {
+//   getAllRecipes,
+//   idName,
+// };
--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,3 +1,243 @@
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createRecipe } from '../../redux/actions';
+// import NavBar from '../../components/NavBar/NavBar';
+// import style from "./Form.module.css";
+
+// const dietOptions = [
+//   "gluten free",
+//   "dairy free",
+//   "lacto ovo vegetarian",
+//   "vegan",
+//   "paleolithic",
+//   "primal",
+//   "whole 30",
+//   "pescatarian",
+//   "ketogenic",
+//   "fodmap friendly"
+// ];
+
+// const Form = () => {
+//   const dispatch = useDispatch();
+
+//   const [title, setTitle] = useState('');
+//   const [image, setImage] = useState('');
+//   const [summary, setSummary] = useState('');
+//   const [healthScore, setHealthScore] = useState(''); // Initialize healthScore as an empty string
+//   const [step, setStep] = useState('');
+//   const [selectedDiets, setSelectedDiets] = useState([]);
+//   const [errors, setErrors] = useState({
+//     title: true,
+//     image: true,
+//     summary: true,
+//     healthScore: false, // Set initial healthScore error to false
+//     step: true,
+//     diets: true
+//   });
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (validateForm()) {
+//       const recipeData = {
+//         title,
+//         image,
+//         summary,
+//         healthScore: parseInt(healthScore), // Convert healthScore to an integer
+//         step,
+//         diets: selectedDiets,
+//         createInDb: true,
+//       };
+
+//       dispatch(createRecipe(recipeData));
+
+//       setTitle('');
+//       setImage('');
+//       setSummary('');
+//       setHealthScore(''); // Reset healthScore to an empty string
+//       setStep('');
+//       setSelectedDiets([]);
+//       setErrors({
+//         title: false,
+//         image: false,
+//         summary: false,
+//         healthScore: false,
+//         step: false,
+//         diets: false
+//       });
+//     }
+//   };
+
+//   const validateForm = () => {
+//     const formErrors = {
+//       title: title.trim() === '',
+//       image: image.trim() === '',
+//       summary: summary.trim() === '',
+//       healthScore: healthScore.trim() === "" || healthScore <= 0 || healthScore > 100,
+//       step: step.trim() === '',
+//       diets: selectedDiets.length === 0
+//     };
+
+//     setErrors(formErrors);
+
+//     return Object.values(formErrors).every((error) => !error);
+//   };
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     const updatedErrors = { ...errors, [name]: value.trim() === '' };
+//     setErrors(updatedErrors);
+
+//     if (name === 'healthScore') {
+//       setHealthScore(value);
+//       if (value.trim() !== '' && (parseInt(value) <= 0 || parseInt(value) > 100)) {
+//         updatedErrors.healthScore = true;
+//       } else {
+//         updatedErrors.healthScore = false;
+//       }
+//     } else {
+//       switch (name) {
+//         case 'title':
+//           setTitle(value);
+//           break;
+//         case 'image':
+//           setImage(value);
+//           break;
+//         case 'summary':
+//           setSummary(value);
+//           break;
+//         case 'step':
+//           setStep(value);
+//           break;
+//         default:
+//           break;
+//       }
+//     }
+
+//     setErrors(updatedErrors);
+//   };
+
+//   const handleDietChange = (e) => {
+//     const { value, checked } = e.target;
+//     let updatedSelectedDiets = [];
+
+//     if (checked) {
+//       updatedSelectedDiets = [...selectedDiets, value];
+//     } else {
+//       updatedSelectedDiets = selectedDiets.filter((diet) => diet !== value);
+//     }
+
+//     setSelectedDiets(updatedSelectedDiets);
+//     setErrors({ ...errors, diets: updatedSelectedDiets.length === 0 });
+//   };
+
+//   const isFormIncomplete = Object.values(errors).some((error) => error);
+//   const isButtonDisabled =
+//     isFormIncomplete ||
+//     title.trim() === '' ||
+//     image.trim() === '' ||
+//     summary.trim() === '' ||
+//     healthScore.trim() === '' ||
+//     step.trim() === '' ||
+//     selectedDiets.length === 0;
+
+//   return (
+//     <div className={style.container}>
+//       <NavBar />
+//       {isFormIncomplete && (
+//         <span className={style.error}>Please fill in all the fields to create the recipe.</span>
+//       )}
+//       <h1 className={style.titulo}>Create your own recipe and share it</h1>
+//       <form onSubmit={handleSubmit}>
+//         <h1 className={style.tipo}>Title</h1>
+//         <label>
+//           <input
+//             type="text"
+//             name="title"
+//             value={title}
+//             onChange={handleInputChange}
+//           />
+//           {errors.title && <span className={style.error}>Please enter a title</span>}
+//         </label>
+//         <br />
+//         <h1 className={style.tipo}>Image</h1>
+//         <label>
+//           <input
+//             type="text"
+//             name="image"
+//             value={image}
+//             onChange={handleInputChange}
+//           />
+//           {errors.image && <span className={style.error}>Please enter an image URL</span>}
+//         </label>
+//         <br />
+//         <h1 className={style.tipo}>Summary</h1>
+//         <label>
+//           <input
+//             type="text"
+//             name="summary"
+//             value={summary}
+//             onChange={handleInputChange}
+//           />
+//           {errors.summary && <span className={style.error}>Please enter a summary</span>}
+//         </label>
+//         <br />
+//         <h1 className={style.tipo}>Health Score</h1>
+//         <label>
+//           <input
+//             type="number"
+//             name="healthScore"
+//             value={healthScore}
+//             onChange={handleInputChange}
+//             className={errors.healthScore ? style.errorInput : ''}
+//           />
+//           {errors.healthScore && <span className={style.error}>Please enter a valid health score (1-100, not zero)</span>}
+//         </label>
+//         <br />
+//         <h1 className={style.tipo}>Steps</h1>
+//         <label>
+//           <input
+//             type="text"
+//             name="step"
+//             value={step}
+//             onChange={handleInputChange}
+//           />
+//           {errors.step && <span className={style.error}>Please enter the steps</span>}
+//         </label>
+//         <br />
+//         <h1 className={style.tipo}>Diets</h1>
+//         <label></label>
+//         <br />
+//         <div className={style.checkContainer}>
+//           {dietOptions.map((diet) => (
+//             <label key={diet} className={style.check}>
+//               <input
+//                 type="checkbox"
+//                 value={diet}
+//                 checked={selectedDiets.includes(diet)}
+//                 onChange={handleDietChange}
+//               />
+//               {diet}
+//             </label>
+//           ))}
+//         </div>
+//         {errors.diets && <span className={style.error}>Please select at least one diet</span>}
+//         <br />
+//         <button
+//           type="submit"
+//           className={`${style.b1} ${isButtonDisabled ? style.disabled : ''}`}
+//           disabled={isButtonDisabled}
+//         >
+//           Create Recipe
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Form;
+
+
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createRecipe } from '../../redux/actions';
@@ -23,38 +263,27 @@ const Form = () => {
   const [title, setTitle] = useState('');
   const [image, setImage] = useState('');
   const [summary, setSummary] = useState('');
-  const [healthScore, setHealthScore] = useState(0);
+  const [healthScore, setHealthScore] = useState('');
   const [step, setStep] = useState('');
   const [selectedDiets, setSelectedDiets] = useState([]);
   const [errors, setErrors] = useState({
-    title: false,
-    image: false,
-    summary: false,
-    healthScore: false,
-    step: false,
-    diets: false
+    title: true,
+    image: true,
+    summary: true,
+    healthScore: true, // Set initial healthScore error to true
+    step: true,
+    diets: true
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const formErrors = {
-      title: title === '',
-      image: image === '',
-      summary: summary === '',
-      healthScore: healthScore < 1 || healthScore > 100,
-      step: step === '',
-      diets: selectedDiets.length === 0
-    };
-
-    if (Object.values(formErrors).some((error) => error)) {
-      setErrors(formErrors);
-    } else {
+    if (validateForm()) {
       const recipeData = {
         title,
         image,
         summary,
-        healthScore,
+        healthScore: parseInt(healthScore),
         step,
         diets: selectedDiets,
         createInDb: true,
@@ -65,7 +294,7 @@ const Form = () => {
       setTitle('');
       setImage('');
       setSummary('');
-      setHealthScore(0);
+      setHealthScore('');
       setStep('');
       setSelectedDiets([]);
       setErrors({
@@ -79,53 +308,145 @@ const Form = () => {
     }
   };
 
+  const validateForm = () => {
+    const formErrors = {
+      title: title.trim() === '',
+      image: image.trim() === '',
+      summary: summary.trim() === '',
+      healthScore: healthScore.trim() === '' || healthScore < 1 || healthScore > 100,
+      step: step.trim() === '',
+      diets: selectedDiets.length === 0
+    };
+
+    setErrors(formErrors);
+
+    return Object.values(formErrors).every((error) => !error);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const updatedErrors = { ...errors, [name]: value.trim() === '' };
+    setErrors(updatedErrors);
+
+    if (name === 'healthScore') {
+      setHealthScore(value);
+      if (value.trim() !== '') {
+        const score = parseInt(value);
+        if (score < 1 || score > 100) {
+          updatedErrors.healthScore = true;
+        } else {
+          updatedErrors.healthScore = false;
+        }
+      } else {
+        updatedErrors.healthScore = true;
+      }
+    } else {
+      switch (name) {
+        case 'title':
+          setTitle(value);
+          break;
+        case 'image':
+          setImage(value);
+          break;
+        case 'summary':
+          setSummary(value);
+          break;
+        case 'step':
+          setStep(value);
+          break;
+        default:
+          break;
+      }
+    }
+
+    setErrors(updatedErrors);
+  };
+
   const handleDietChange = (e) => {
     const { value, checked } = e.target;
+    let updatedSelectedDiets = [];
+
     if (checked) {
-      setSelectedDiets([...selectedDiets, value]);
+      updatedSelectedDiets = [...selectedDiets, value];
     } else {
-      setSelectedDiets(selectedDiets.filter((diet) => diet !== value));
+      updatedSelectedDiets = selectedDiets.filter((diet) => diet !== value);
     }
+
+    setSelectedDiets(updatedSelectedDiets);
+    setErrors({ ...errors, diets: updatedSelectedDiets.length === 0 });
   };
+
+  const isFormIncomplete = Object.values(errors).some((error) => error);
+  const isButtonDisabled =
+    isFormIncomplete ||
+    title.trim() === '' ||
+    image.trim() === '' ||
+    summary.trim() === '' ||
+    healthScore.trim() === '' ||
+    step.trim() === '' ||
+    selectedDiets.length === 0;
 
   return (
     <div className={style.container}>
       <NavBar />
-      <h1 className={style.titulo}>create your own recipe and share it</h1>
+      {isFormIncomplete && (
+        <span className={style.error}>Please fill in all the fields to create the recipe.</span>
+      )}
+      <h1 className={style.titulo}>Create your own recipe and share it</h1>
       <form onSubmit={handleSubmit}>
         <h1 className={style.tipo}>Title</h1>
         <label>
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleInputChange}
+          />
           {errors.title && <span className={style.error}>Please enter a title</span>}
         </label>
         <br />
         <h1 className={style.tipo}>Image</h1>
         <label>
-          <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+          <input
+            type="text"
+            name="image"
+            value={image}
+            onChange={handleInputChange}
+          />
           {errors.image && <span className={style.error}>Please enter an image URL</span>}
         </label>
         <br />
         <h1 className={style.tipo}>Summary</h1>
         <label>
-          <input type="text" value={summary} onChange={(e) => setSummary(e.target.value)} />
+          <input
+            type="text"
+            name="summary"
+            value={summary}
+            onChange={handleInputChange}
+          />
           {errors.summary && <span className={style.error}>Please enter a summary</span>}
         </label>
         <br />
         <h1 className={style.tipo}>Health Score</h1>
-        <h3 className={style.uno}>1 - 100</h3>
         <label>
           <input
             type="number"
+            name="healthScore"
             value={healthScore}
-            onChange={(e) => setHealthScore(e.target.valueAsNumber)}
+            onChange={handleInputChange}
             className={errors.healthScore ? style.errorInput : ''}
           />
-          {errors.healthScore && <span className={style.error}>Please enter a health score between 1 and 100</span>}
+          {errors.healthScore && <span className={style.error}>Please enter a valid health score (1-100)</span>}
         </label>
         <br />
         <h1 className={style.tipo}>Steps</h1>
         <label>
-          <input type="text" value={step} onChange={(e) => setStep(e.target.value)} />
+          <input
+            type="text"
+            name="step"
+            value={step}
+            onChange={handleInputChange}
+          />
           {errors.step && <span className={style.error}>Please enter the steps</span>}
         </label>
         <br />
@@ -147,7 +468,11 @@ const Form = () => {
         </div>
         {errors.diets && <span className={style.error}>Please select at least one diet</span>}
         <br />
-        <button type="submit" className={style.b1}>
+        <button
+          type="submit"
+          className={`${style.b1} ${isButtonDisabled ? style.disabled : ''}`}
+          disabled={isButtonDisabled}
+        >
           Create Recipe
         </button>
       </form>
